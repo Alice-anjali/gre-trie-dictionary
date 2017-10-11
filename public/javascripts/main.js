408f5fe3d24993bb5ac4010a3edce607c0dd9ad2
Rishav Agarwal <rishav.159@gmail.com>
2017-10-11 21:13:58 +0530

    Adds search word

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -14,3 +14,33 @@ function search_word(trie,word){
     return undefined;
   }
 }
+
+function get_all_words(prefix,trie){
+  var words = {};
+  if('_end_' in trie){
+    words[prefix] = trie['_meaning_'];
+  }
+  for(key in trie) {
+    if(key != '_end_' && key != '_meaning_') {
+      var newwords = get_all_words(prefix+key,trie[key]);
+      for(key in newwords){
+        words[key] = newwords[key]
+      }
+    }
+  }
+  return words;
+}
+
+function search_prefix_words(trie,prefix) {
+  var curr = trie;
+  var words = [];
+  for(var i = 0; i < prefix.length; i++) {
+    var letter = prefix[i];
+    if(letter in curr) {
+      curr = curr[letter];
+    }else{
+      return words;
+    }
+  }
+  return get_all_words(prefix,curr);
+}
